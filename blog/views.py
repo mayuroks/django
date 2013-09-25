18b51e867b492a98da5b57959da6017356e77a0e
mayur <mayur@mayur.(none)>
2013-09-25 06:35:46 +0530

    CB-views, context n templates, url tags

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# Import your generic views here.
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+# Import your Tables/ Models
+from blog.models import Post
+
+# Make your view classes
+class BlogListView(ListView):
+	model = Post
+	template_name = "index.html"	# Magic happens here
+
+class PostDetailView(DetailView):
+	model = Post
+	template_name = "details.html"	# need to understand more about context
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+import blog.views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'microblog.views.home', name='home'),
+    url(r'^$', blog.views.BlogListView.as_view(), name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^blog/(?P<pk>\d+)/$", blog.views.PostDetailView.as_view(), name="post-details"),
 )
