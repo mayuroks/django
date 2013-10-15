37ac25abcfc0f8a4b0d9a9ccb9a7786102812416
mayuroks <mayuroks@gmail.com>
2013-10-16 02:07:51 +0530

    form clean override

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,20 @@
 from django import forms
 from blog.models import Post
-
+from django.template.defaultfilters import slugify 
 class FormPost(forms.ModelForm):
 	class Meta:
 		model = Post
 
 	def clean(self):
-		self.fields['title'] = self.cleaned_data.get('title') + 'CLEAN_WORK'
-		return self.cleaned_data
+		cleaned_data = super(FormPost, self).clean()
+		title = cleaned_data.get('title') + '__from_form'
+		cleaned_data['title'] = title
 
+		slug = cleaned_data.get('slug')
+		if not slug:
+			cleaned_data['slug'] = slugify(title)
+		return cleaned_data
+##https://docs.djangoproject.com/en/1.5/ref/forms/validation/#form-and-field-validation
 class UpdatePost(forms.ModelForm):
 	class Meta:
 		model = Post
