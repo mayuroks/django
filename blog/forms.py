6cfb4d67b0cb6ef0dc44707039cc81c3846e11c8
Mayur Rokade <mayuroks@gmail.com>
2013-10-15 22:58:14 +0530

    forms exclude

--- blog/forms.py
+++ blog/forms.py
@@ -5,6 +5,10 @@ class FormPost(forms.ModelForm):
 	class Meta:
 		model = Post
 
+	def clean(self):
+		self.fields['title'] = self.cleaned_data.get('title') + 'CLEAN_WORK'
+		return self.cleaned_data
+
 class UpdatePost(forms.ModelForm):
 	class Meta:
 		model = Post
